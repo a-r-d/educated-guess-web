5bc7f166519b1d796bce8759e9510cbcbda1c565
Aaron Decker <d00ble.ard@gmail.com>
2013-07-24 00:18:36 -0400

    added static files

---
+++ public/custom.js
@@ -0,0 +1,30 @@
+// The guess JS.
+
+var _the_guess_ = new Object();
+
+
+
+
+_the_guess_.submitWordList = function(){
+
+}
+
+_the_guess_.addWord = function(){
+
+}
+
+_the_guess_.removeWord = function(){
+
+}
+
+_the_guess_.getSuggestion = function(){
+
+}
+
+
+
+
+
+$(document).ready( function(){
+
+});

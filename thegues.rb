0e6bd214a76e71a041a6b7cbb9825dbdc01f536a
Aaron Decker <d00ble.ard@gmail.com>
2013-07-24 19:45:37 -0400

    added home page

--- thegues.rb
+++ thegues.rb
@@ -3,9 +3,17 @@
 #  on given known information and prefs.
 
 require 'sinatra'
+require 'haml'
+
+## Haml Options:
+#Haml::Options.defaults[:format] = :html5
+configure do
+  set :name, 'educated-guess'
+  set :haml, { :format => :html5 }
+end
 
 get '/' do 
-    'Hullo werld'
+    haml :home, :locals => {:wordlists => [{:name=> "test"}, {:name=> "test2"}]}
 end
 
 post '/' do 

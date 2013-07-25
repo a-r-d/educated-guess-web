e9d97859027d51efce17233d736887acfc174e8a
Aaron Decker <d00ble.ard@gmail.com>
2013-07-24 21:52:50 -0400

    starting in on actions

--- thegues.rb
+++ thegues.rb
@@ -1,7 +1,6 @@
 # Educated Guess
 #  educated guess will generate password lists based 
 #  on given known information and prefs.
-
 require 'sinatra'
 require 'haml'
 
@@ -12,14 +11,44 @@ configure do
   set :haml, { :format => :html5 }
 end
 
-get '/' do 
-    haml :home, :locals => {:wordlists => [{:name=> "test"}, {:name=> "test2"}]}
+
+$wordlist_dir = Dir.pwd + "/wordlist" # (relative)
+$skip_list = [".", ".."]
+
+
+def nameOK?(fullpath)
+	$skip_list.each { |s| 
+		if File.basename(fullpath) == s then
+			return false
+		end
+	}
+	return true
 end
+
+
+get '/' do 
+	wordlist = []
+	Dir.foreach($wordlist_dir) { |file| 
+		the_path =  $wordlist_dir + "/" + file
+
+		if !nameOK?(the_path) then
+			next
+		end
+		
+		wordlist << {
+			:name=> File.basename(the_path),
+			:size=> (File.size(the_path) / 1000)
+		}
+	}
+
+    haml :home, :locals => {:wordlists => wordlist}
+end
+
 
 post '/' do 
     'hullo post'
 end
 
- 
+
 
 
